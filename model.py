b418fc7445e550782e54b33fe445dd847b4c61bf
Sanjana Bhat <sanjanabhat2103@gmail.com>
2026-08-11 18:02:37 +0530

    Step 4: predict_from_scores

--- model.py
+++ model.py
@@ -32,8 +32,11 @@ def compute_scores(x, params):
     bias = params['b']
     return np.asarray(x @ weights + bias, dtype = float)
 
-# Step 4 - predict_from_scores (not yet solved)
-# TODO: implement
+# Step 4 - predict_from_scores
+import numpy as np
+
+def predict_from_scores(scores):
+    return np.where(scores >= 0, 1, -1)
 
 # Step 5 - hinge_loss_example (not yet solved)
 # TODO: implement
